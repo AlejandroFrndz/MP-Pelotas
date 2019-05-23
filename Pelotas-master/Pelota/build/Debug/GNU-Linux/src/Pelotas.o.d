85599e7fbd21ab2f80319fa3928506c0f9aea19e
Alejandro Fernandez Alcaide <47327793+AlejandroFrndz@users.noreply.github.com>
2019-05-23 16:43:15 +0200

    Delete Pelotas.o.d

--- Pelotas-master/Pelota/build/Debug/GNU-Linux/src/Pelotas.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/GNU-Linux/src/Pelotas.o: src/Pelotas.cpp include/Pelotas.h \
- include/Pelota.h include/definiciones.h
-
-include/Pelotas.h:
-
-include/Pelota.h:
-
-include/definiciones.h:
